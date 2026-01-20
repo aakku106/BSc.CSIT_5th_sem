7c68fb399d07586db3dbe98c2766e71402808ef4
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-01-20 07:22:35 +0545

    d5

---
+++ WT/practises/day5/script.js
@@ -0,0 +1,10 @@
+const body = document.getElementById("div");
+body.style.height = "90vh";
+body.style.width = "90vw";
+body.style.borderRadius = "12px";
+body.style.margin = "auto";
+body.style.backgroundColor = "red";
+
+body.addEventListener(("click") =>{
+
+} )
