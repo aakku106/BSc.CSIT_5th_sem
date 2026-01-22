9938aec65c3c8e321cfece7644b115fe98a6ef0b
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-01-22 08:29:59 +0545

    Implemented the proper methods to delete specific element from queue

--- WT/practises/day5/script.js
+++ WT/practises/day5/script.js
@@ -12,6 +12,7 @@ const addStyles = () => {
   body.style.width = "90vw";
   body.style.borderRadius = "12px";
   body.style.margin = "auto";
+  body.style.marginRight = "auto";
   body.style.backgroundColor = "rgb(229, 179, 155)";
   body.style.textAlign = `center`;
   body.style.padding = `1rem`;
@@ -33,7 +34,7 @@ const addListContainer = () => {
 const addList = () => {
   if (input.value.trim()) {
     todoData.push(input.value);
-    localStorage.setItem("TO_DO_DATA", JSON.stringify(todoData));
+    localStorage.setItem("TO_DO_DATA", JSON.stringify(todoData.reverse()));
     input.value = "";
     renderList();
   }
@@ -42,21 +43,31 @@ const renderList = () => {
   listContainer.innerHTML = "";
   todoData.forEach((value, index) => {
     const list = document.createElement("li");
+    let p = document.createElement("p");
     const delBtn = document.createElement("button");
-    list.textContent = value;
+    p.textContent = value;
     listContainer.append(list);
+    list.append(p);
     list.append(delBtn);
-    handleDel(delBtn);
+    handleDel(delBtn, index);
   });
   console.log(todoData);
   console.log(JSON.parse(localStorage.getItem("TO_DO_DATA")));
 };
-const handleDel = (delBtn) => {
+const handleDel = (delBtn, index) => {
   delBtn.style.color = "red";
   delBtn.innerText = "DELETE";
+  delBtn.style.marginLeft = "45%";
+  delBtn.addEventListener("click", () => {
+    deleteList(delBtn, index);
+  });
 };
-const deleteList = (index) => {
-  listContainer.innerHTML = "";
+const deleteList = (delBtn, index) => {
+  console.log("wanna delete: ", delBtn, "of index: ", index);
+  delBtn.removeEventListener("click", deleteList);
+  todoData.splice(index, 1);
+  localStorage.setItem("TO_DO_DATA", JSON.stringify(todoData));
+  renderList();
 };
 const clearAllToDO = () => {
   localStorage.clear("TO_DO_DATA");
