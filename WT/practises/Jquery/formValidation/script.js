e10a617fa121228c1cfd99bc0f4588b6f9d377b3
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-02-03 08:16:44 +0545

    errorhandle

--- WT/practises/Jquery/formValidation/script.js
+++ WT/practises/Jquery/formValidation/script.js
@@ -2,3 +2,16 @@
 // age--> num, 12>age<90
 // email
 // gender: 2 options
+//
+
+const form = document.getElementById("form");
+const err = {};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("vslidating name");
+  const name = e.target[0].value.trim();
+  if (name.length < 4) {
+    Error;
+  }
+});
