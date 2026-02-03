a2502b136c42424be4ff968d7e349c1c6f11a739
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-02-03 08:32:27 +0545

    functional

--- WT/practises/Jquery/formValidation/script.js
+++ WT/practises/Jquery/formValidation/script.js
@@ -9,14 +9,18 @@ const err = {};
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("vslidating name");
   const name = e.target[0].value.trim();
-  console.log(name);
-  if (name.length < 4) err["name"] = "Name must be longer than 3 characters.";
-  else err.name = "";
+  handleName(name);
+});
+const handleName = (name) => {
+  if (name.length < 4) err.name = "Name must be longer than 3 characters.";
+  //   else err.name = "";
+
+  if (name.length > 20) err.name = "Name must be shorter than 20 character";
+  //   else err.name = "";
 
   if (err && "name" in err) {
-    console.log("short name baka");
     document.getElementsByClassName("name-err")[0].textContent = err.name;
+    err.name = "";
   }
-});
+};
