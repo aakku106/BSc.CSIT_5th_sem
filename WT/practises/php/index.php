05d0a0eb151494510fd39df091a78bf08b53df08
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-03-19 07:32:12 +0545

    php 1st try

---
+++ WT/practises/php/index.php
@@ -0,0 +1,24 @@
+<h1> weeeeeeee </h1>
+<?php
+$cat= "meoww";
+echo $cat;
+?>
+
+<h1> 
+	<?php
+$cat= "o yaaaaaaaaaaaaaa!!!!!!!!!!!";
+echo $cat;
+?>
+</h1>
+
+<h1> 
+	<?php
+	$age = 22;
+
+	if ($age > 20) {
+	echo "Adult: User of age: $age";
+	}else{
+	echo "Minor: : User of age: $age";
+	}
+	?>
+</h1>
