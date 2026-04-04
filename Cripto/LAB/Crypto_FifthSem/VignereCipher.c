8e45047a3934ca21414f050ebae6f3f50bd3a64c
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-04-04 16:04:23 +0545

    critto labs

---
+++ Cripto/LAB/Crypto_FifthSem/VignereCipher.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+// ##include<conio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+    void encipher();
+void decipher();
+void main()
+{
+    int choice;
+    // clrscr();
+    while (1)
+    {
+        printf("\n1. Encrypt Text");
+        printf("\t2. Decrypt Text");
+        printf("\t3. Exit");
+        printf("\n\nEnter Your Choice : ");
+        scanf("%d", &choice);
+        if (choice == 3)
+            exit(0);
+        else if (choice == 1)
+            encipher();
+        else if (choice == 2)
+            decipher();
+        else
+           { printf("Please Enter Valid Option.");
+            exit(0);}
+    }
+}
+
+void encipher()
+{
+    unsigned int i, j;
+    char input[50], key[10];
+
+    printf("\nEnter Plain Text: ");
+    scanf("%s", input);
+
+    printf("\nEnter Key Value: ");
+    scanf("%s", key);
+
+    printf("\nResultant Cipher Text: ");
+    for (i = 0, j = 0; i < strlen(input); i++, j++)
+    {
+        if (j >= strlen(key))
+        {
+            j = 0;
+        }
+        printf("%c", 65 + (((toupper(input[i]) - 65) + (toupper(key[j]) - 65)) % 26));
+    }
+}
+
+
+void decipher()
+{
+    unsigned int i, j;
+    char input[50], key[10];
+    int value;
+
+    printf("\nEnter Cipher Text: ");
+    scanf("%49s", input);
+
+    printf("\nEnter the key value: ");
+    scanf("%9s", key);
+
+    printf("\nResultant Plain Text: ");
+
+    for (i = 0, j = 0; i < strlen(input); i++, j++)
+    {
+        if (j >= strlen(key))
+        {
+            j = 0;
+        }
+
+        value = (toupper(input[i]) - 65) - (toupper(key[j]) - 65);
+
+        if (value < 0)
+        {
+            value += 26;
+        }
+
+        printf("%c", 65 + (value % 26));
+    }
+}
